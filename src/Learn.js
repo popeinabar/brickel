da20babaa32ebb5687da27970471d9d9c4996a66
Ayush raj <103944161+popeinabar@users.noreply.github.com>
2023-03-06 21:58:25 +0530

    Add files via upload

---
+++ src/Learn.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Serch from './component/Serch.js'
+
+const Learn = () => {
+  return (
+    <>
+        <div>
+          
+          <Serch/>
+        </div>
+    </>
+  )
+}
+
+export default Learn
