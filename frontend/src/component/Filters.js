a6e421fb347bd65e4d12f29bd7e74dccc30b7aa4
Ayush raj <ayushphenomal1@gmail.com>
2023-04-15 10:41:52 +0530

    Created Search and filter compoennt and imported them in Learn and teach

---
+++ frontend/src/component/Filters.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import './Filters.css'
+//
+import Serch from '../component/Serch'
+
+//
+import { useState, createContext } from 'react';
+
+const valuesContext =createContext();
+
+
+const Filters = (props) => {
+  const [timing, setTiming] = useState('');
+  const [subject, setSubject] = useState('');
+  const [topic, setTopic] = useState('');
+
+  const handleTimingChange = (event) => {
+    setTiming(event.target.value);
+  }
+  
+
+  const handleSubjectChange = (event) => {
+    setSubject(event.target.value);
+  }
+
+  const handleTopicChange = (event) => {
+    setTopic(event.target.value);
+  }
+  const lowerCaseTiming = timing.toLowerCase();
+  const lowerCaseSubject = subject.toLowerCase();
+  const lowerCaseTopic = topic.toLowerCase();
+  
+
+
+  const {filteredData, setFilteredData} = props;
+
+  
+  
+  const handleSubmit = (event) => { 
+    event.preventDefault();
+    let newFilteredData = {  timing: lowerCaseTiming, 
+      subject: lowerCaseSubject, 
+      topic: lowerCaseTopic };
+      setFilteredData(newFilteredData)
+      props.setTrigger(false)
+    
+  }
+  
+  return (props.trigger) ? (
+    <>
+    {/* <valuesContext.Provider value={filteredData}>
+
+    </valuesContext.Provider> */}
+      <div className='popup'>
+        <div className='popup-inner'>
+          <div className='head'>
+            <h1>Filter for teachers</h1>
+            <button className='button-close' onClick={()=> props.setTrigger(false)}> close</button> 
+          </div>
+          {props.children}
+          <form className='fil-form' onSubmit={handleSubmit}>
+            <ul>
+              <li className='fil-li'>
+                timing: <input className='fil-time' type="time" name="timing" value={timing} onChange={handleTimingChange} />
+              </li>
+              <li className='fil-li'>
+                subject: <input className='fil-text' type="text" name="subject" value={subject} onChange={handleSubjectChange} />
+              </li>
+              <li className='fil-li'>
+                topic: <input className='fil-text' type="text" name="topic" value={topic} onChange={handleTopicChange} />
+              </li>
+            </ul>
+            <button className='smit' type="submit" >Filtering</button>
+          </form>
+        </div>
+      </div>
+            
+    </>
+
+  ) : "";
+}
+
+
+export default Filters
+export { valuesContext }; 
--- frontend/src/component/Search.js
+++ frontend/src/component/Search.js
@@ -1,53 +1,103 @@
 import React from 'react'
-import image_info from '../assets/img1.jpg'
-import filter from '../assets/filter.png'
-import './Search.css'
+import './Serch.css'
+import { Link } from 'react-router-dom';
 
-const Search = () => {
+ 
+
+const Serch = (props) => {
+    const timing = props.filteredData.timing
+    const subject = props.filteredData.subject
+    const topic = props.filteredData.topic
+  console.log(props.any)
+  // console.log(timing);
+  // console.log(props.filteredData.subject)
   return (
     <> 
-        <div className='search'>
-            <div className='top'>
-                <h1>Hi, Lerner</h1>
-                <img className='img-filter' src={filter} alt='lerner-tutor'></img>
-            </div>
+      {      
+                
+        props.any.filter((user)=>{
+          
+          if(props.isTeaching===false){
+            console.log(user.LTiming.toLowerCase)
+            if (timing ==='' && subject==='' && topic===''){
+              return user
+            }else if(
+              (user.LTiming.toLowerCase().includes(timing)) && (user.LSubject.toLowerCase().includes(subject)) && (user.LTopic.toLowerCase().includes(topic)) 
+              ){
+                return user
+              }
+            }
+            else if(props.isTeaching===true){
+            console.log(user)
+            if (timing ==='' && subject==='' && topic===''){
+              return user
+            }else if(
+              (user.TTiming.toLowerCase().includes(timing)) && (user.TSubject.toLowerCase().includes(subject)) && (user.TTopic.toLowerCase().includes(topic)) 
+            ){
+              return user
+            }
+          }
+        }).map((user, key)=>{
+            
+            return(
+            <div className='total'>
+                        
             <div className='info'>
-                <img className='image-info' src={image_info} alt='lerner-tutor'></img>
+                <div className='image-div'>
+                    <img className='image-info' src={user.image} alt='lerner-tutor'></img>{/*taking teacher and student image*/}
+                </div>
                 <ul className='flex'>
+                        
                     
                     <div className='flex1'>
-                    <li>
-                        Name:
-                    </li>
-                    <li>
-                        Subject:
-                    </li>
-                    <li>
-                        topic:
-                    </li>
-                    <li>
-                        Timing:10:55 to 11:55 
-                    </li>
+                            <li>
+                                
+                                {console.log(props)}
+                                {props.name}: {user.Name}
+                            </li>
+                            <li>
+                              
+                                {props.subject}: {props.isTeaching?user.TSubject:user.LSubject}
+                            </li>
+                            <li>
+                                {props.topic}: {props.isTeaching?user.TTopic:user.LTopic}
+                            </li>
+                            <li>
+                                {props.timing}: {props.isTeaching?user.TTiming:user.LTiming} 
+                            </li>
+                                
                     </div>
-                    <div className='flex2'>
-                    <li>
-                        Rating:
-                    </li>
-                    <li>
-                        Occupation:
-                    </li>
-                    <li>
-                        Impression: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500
-                    </li>
+                    <div className='flex4'>
+                        <div className='flex2'>
+                            {/* <li>
+                                {props.likes} {user.likes}
+                            </li> */}
+                            <li>
+                                {props.occupation}: {user.Occupation} 
+                            </li>
+                            <li className='imp1'>
+                                {props.impression}: {user.Impression}
+                            </li>
+                        </div>
+                        
+                        <div className='flex3'>
+                            <Link to={'/https://mail.google.com/mail/u/0/'}>
+                                <button className='mailbtn'>Connect</button>
+                            </Link>
+                        </div>
                     </div>
-                    
                 </ul>
-
-            </div>
-        </div>
+                </div>
+                <li className='imp2'>
+                                {props.impression}: {user.impression}
+                </li>
+                `</div>
+        )
+        })}
+    
     </>
 
   )
 }
 
-export default Search
+export default Serch
