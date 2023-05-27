f91a44cd94ec67caa81b06d6565acc6d06f52228
Ayush raj <ayushphenomal1@gmail.com>
2023-05-27 14:48:38 +0530

    Updated the logout and fetch

--- frontend/src/component/Search.js
+++ frontend/src/component/Search.js
@@ -3,113 +3,89 @@ import "./Search.css";
 import { Link } from "react-router-dom";
 
 const Serch = (props) => {
-  const timing = props.filteredData.timing;
-  const subject = props.filteredData.subject;
-  const topic = props.filteredData.topic;
+  const timingf = props.filteredData.timing;
+  const subjectf = props.filteredData.subject;
+  const topicf = props.filteredData.topic;
   console.log(props.any[0].Image.url);
   // console.log(timing);
   // console.log(props.filteredData.subject)
   return (
     <>
     {/* filtering process */}
-      {props.any
-        .filter((user) => {
-          if (props.isTeaching === false) {
-            console.log(user.LTiming.toLowerCase);
-            if (timing === "" && subject === "" && topic === "") {
-              return user;
-            } else if (
-              user.TTiming.toLowerCase().includes(timing) &&
-              user.TSubject.toLowerCase().includes(subject) &&
-              user.TTopic.toLowerCase().includes(topic)
-            ) {
-              return user;
-            }
-          } else if (props.isTeaching === true) {
-            if (timing === "" && subject === "" && topic === "") {
-              return user;
-            } else if (
-              user.LTiming.toLowerCase().includes(timing) &&
-              user.LSubject.toLowerCase().includes(subject) &&
-              user.LTopic.toLowerCase().includes(topic)
-            ) {
-              return user;
-            }
-          }
-        })
-        .map((user, key) => {
-          {
-            console.log(user);
-          }
-          return (
-           //conponent
-           <div className="total">
-              <div className="info">
-                <div className="image-div">
-                  <img
-                    className="image-info"
-                    // src={props.any[0].Image.url}
-                    src={user.Image.url}
-                    alt="lerner-tutor"
-                  ></img>
-                  {/*taking teacher and student image*/}
-                </div>
-                <ul className="flex">
-                  <div className="flex1">
-                    <li>
-                      {/* {console.log(props)} */}
-                      {props.name}:{" "}{user.Name}
-                    </li>
-                    <li>
-                      {props.subject}:{" "}{props.isTeaching ? user.LSubject : user.TSubject}
+    {
+  props.any
+    .filter((user) => {
+      const timing = props.isTeaching ? user.LTiming : user.TTiming;
+      const subjects = props.isTeaching ? user.LSubject : user.TSubject;
+      const topics = props.isTeaching ? user.LTopic : user.TTopic;
 
-                    </li>
-                    <li>
-                     {props.topic}:{" "}
-                    
-                        {props.isTeaching ? user.LTopic : user.TTopic}
-                      
-                    </li>
-                    <li>
-                       {props.timing}:{" "}
-                    
-                        {props.isTeaching ? user.LTiming : user.TTiming}{" "}
-                      
-                    </li>
-        
-                  </div>
-                  <div className="flex4">
-                    <div className="flex2">
-                      {/* <li>
-                                {props.likes} {user.likes}
-                            </li> */}
-                      <li>
-                        {props.occupation}: {user.Occupation}
-                      </li>
-                      <li className="imp1">
-                        {props.impression}: {user.Impression}
-                      </li>
-                    </div>
+      if (timingf === "" && subjectf === "" && topicf === "") {
+        return true;
+      } else if (
+        timing.toLowerCase().includes(timingf) &&
+        subjects.some((subject) => subject.toLowerCase().includes(subjectf)) &&
+        topics.some((topic) => topic.toLowerCase().includes(topicf))
+      ) {
+        return true;
+      }
 
-                    <div className="flex3">
-                      <a
-                        href={"mailto:" + user?.Email}
-                        className="connect"
-                        Connect
-                      >
-                        {console.log(user?.Email)}
-                        Connect
-                      </a>
-                    </div>
-                  </div>
-                </ul>
-              </div>
-              <li className="imp2">
-              {props.impression}: {user.Impression}
+      return false;
+    })
+    .map((user, key) => (
+      <div className="total" key={key}>
+        <div className="info">
+          <div className="image-div">
+            <img
+              className="image-info"
+              src={user.Image.url}
+              alt="learner-tutor"
+            />
+          </div>
+          <ul className="flex">
+            <div className="flex1">
+              <li>
+                {props.name}: {user.Name}
+              </li>
+              <li>
+                {props.subject}:{" "}
+                {props.isTeaching
+                  ? user.LSubject.map((item) => `'${item.subject}'`).join(", ")
+                  : user.TSubject.map((item) => `'${item.subject}'`).join(", ")}
+              </li>
+              <li>
+                {props.topic}:{" "}
+                {props.isTeaching
+                  ? user.LTopic.map((item) => `'${item.Chapter}'`).join(", ")
+                  : user.TTopic.map((item) => `'${item.Chapter}'`).join(", ")}
+              </li>
+              <li>
+                {props.timing}: {props.isTeaching ? user.LTiming : user.TTiming}{" "}
               </li>
             </div>
-          );
-        })}
+            <div className="flex4">
+              <div className="flex2">
+                <li>
+                  {props.occupation}: {user.Occupation}
+                </li>
+                <li className="imp1">
+                  {props.impression}: {user.Impression}
+                </li>
+              </div>
+              <div className="flex3">
+                <a href={"mailto:" + user?.Email} className="connect">
+                  Connect
+                </a>
+              </div>
+            </div>
+          </ul>
+        </div>
+        <li className="imp2">
+          {props.impression}: {user.Impression}
+        </li>
+      </div>
+    ))
+}
+
     </>
   );
 };
